c19923d2829e7ba49710f83dcfbbca521a631d07
matheus <matheus@pop-os.localdomain>
2022-08-01 21:43:05 -0300

    Aula de 1 de Agosto de 2022

--- abastecimento/src/App.java
+++ abastecimento/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Quanto custa a gasolina?");
+        float precoGasolina = ler.nextFloat();
+        System.out.println("Quantos reais de gasolina deseja colocar?");
+        float valorAbastecido = ler.nextFloat();
+
+        float litrosTotal = valorAbastecido / precoGasolina  ;
+
+        System.out.println("Você conseguiu abastecer " + litrosTotal + "L com esse valor");
+
     }
 }
